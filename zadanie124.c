57a039de50591a517461534a3fff4ca56e80bb31
JakubGorecki <kuba161295@wp.pl>
2021-04-19 20:28:13 +0200

    dodano mozliwosc liczenia pola trojkata

--- zadanie124.c
+++ zadanie124.c
@@ -1,18 +1,52 @@
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
- int main ()
- {
-     float a;
-     float b;
-     float pole_prostokata;
+float main()
+{
+    for(;;)
+{
+        int c=68 ;
+        int a;
+        int b;
+        int wynik;
+        int pods;
+        int h;
+        int wynik2;
+        int t;
 
-     printf("Podaj dlugosc boku a:");
-     scanf("%f" , &a);
+        printf("prosze wybrac jedna z opcji\n");
+        printf("1.oblicz pole prostokata\n");
+        printf("2.oblicz pole trojkata\n");
+        printf("3.zamknij program\n");
+        scanf("%d", &c);
 
-     printf("Podaj dlugosc boku b:");
-     scanf("%f" , &b);
+        switch (c)
 
-     pole_prostokata = a*b;
-     printf("Pole prostokata wynosi: %f" , pole_prostokata);
-     return 0;
- }
+{
+
+case 1:
+
+    printf("wprowadz dlugosc boku a\n");
+    scanf("%d", &a);
+    printf("wprowadz dlugosc boku b\n");
+    scanf("%d", &b);
+    wynik = a*b;
+    printf("wynik=" "%d\n", wynik);
+
+    break;
+case 2:
+
+    printf("wprowadz dlugosc podstawy trojkata\n");
+    scanf("%d", &pods);
+    printf("wprowadz wysokosc trojkata\n");
+    scanf("%d", &h);
+    wynik2 = 0.5*pods*h;
+    printf("wynik=" "%d\n", wynik2);
+    break;
+
+case 3:
+    exit;
+}
+    return 0;
+    }
+}
